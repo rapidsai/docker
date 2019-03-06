e4ce0fbceaf1b0426eb423b3e7477c9e530f29b1
rlratzel <rlratzel@ratzel.com>
2019-03-06 14:30:42 -0600

    Added initial genBuildScript command - still need to update util script called by template, and test.

---
+++ commands/genBuildScript.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+set -e
+
+# Assume this script is in a subdir of the dir containing rapidstool.sh
+THISDIR=$(dirname $0)
+RAPIDSDEVTOOL_DIR=${THISDIR}/..
+pushd ${RAPIDSDEVTOOL_DIR} > /dev/null
+RAPIDSDEVTOOL_DIR=$(pwd)
+popd > /dev/null
+
+source ${THISDIR}/utils/common.sh
+
+DEBUGFLAG=""
+TEMPL_FILE_NAME=${SCRIPTS_TEMPL_DIR}/build.sh.template
+OUTPUT_FILE_NAME=${RAPIDSDEVTOOL_DIR}/build.sh
+
+SHORTHELP="$0 [-h|-H] -t <templateName> [-o <outputFileName>]"
+LONGHELP="${SHORTHELP}
+   Generate a script to build the RAPIDS components specified in
+   ${CONFIG_FILE_NAME}
+
+   Use -o <outputFileName> to specify the name of the generated build script,
+   but if not specified, the default generated build script will be named
+   \"${OUTPUT_FILE_NAME}\" "
+
+while getopts ":hHdo:" option; do
+    case "${option}" in
+        h)
+            echo "${SHORTHELP}"
+            exit 0
+            ;;
+        H)
+            echo "${LONGHELP}"
+            exit 0
+            ;;
+        d)
+            DEBUGFLAG=-d
+            ;;
+        o)
+            OUTPUT_FILE_NAME=${OPTARG}
+            ;;
+	*)
+	    echo "${SHORTHELP}"
+	    exit 1
+    esac
+done
+
+# TODO: check that OUTPUT_FILE_NAME can be written
+${COMMANDSUTILS_DIR}/genfile.sh ${DEBUGFLAG} ${TEMPL_FILE_NAME} > ${OUTPUT_FILE_NAME}
