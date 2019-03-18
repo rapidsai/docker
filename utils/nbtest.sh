1980120652c27e8a0d2a691e796caf0c76020082
Rick Ratzel <rratzel@nvidia.com>
2019-03-18 08:10:47 -0700

    Update notebooks branch to branch-0.6-release. Added nbtest.sh script to assist with notebook testing (this script is untested!).

---
+++ utils/nbtest.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+EXITCODE=0
+
+for nb in $*; do
+    NBFILENAME=$1
+    NBNAME=$(echo ${NBFILENAME}|cut -d'.' -f1)
+    NBTESTFILENAME=/tmp/${NBNAME}-test.ipynb
+    NBTESTSCRIPT=/tmp/${NBNAME}-test.py
+    shift
+
+    echo --------------------
+    echo ${NBNAME}
+    echo --------------------
+    cat ${NBFILENAME} | grep -v "%%" > ${NBTESTFILENAME}
+    jupyter nbconvert --to script ${NBTESTFILENAME} --output /tmp/${NBNAME}-test
+    echo "Running \"python ${NBTESTSCRIPT}\":"
+    echo 
+    time python ${NBTESTSCRIPT}
+    NBEXITCODE=$?
+    echo EXIT CODE: ${NBEXITCODE}
+    echo
+    EXITCODE=$((EXITCODE | ${NBEXITCODE}))
+done
+
+exit ${EXITCODE}
