9f9548d2fc38f386a749da8eb0ee18a68bef21b0
Rick Ratzel <rratzel@nvidia.com>
2019-03-18 20:41:29 -0700

    Reverted (again) the JIT compile step since the build stage has no driver access, which was discovered to be a requirement for JIT compile for now.

--- utils/build-cuml.sh
+++ utils/build-cuml.sh
@@ -14,7 +14,6 @@ cmake -DCMAKE_INSTALL_PREFIX=$CONDA_PREFIX \
       -DCMAKE_BUILD_TYPE=release .. && \
 make -j && make -j install && \
 cd ../../python && python setup.py build_ext --inplace ${MULTIGPU_OPTION} && \
-python setup.py install ${MULTIGPU_OPTION} && \
-python -c "import cuml"
+python setup.py install ${MULTIGPU_OPTION}
 
 
