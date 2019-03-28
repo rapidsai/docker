850e8f0a3c5af6803417558fdaf3ee2de67ec4ba
Rick Ratzel <rratzel@nvidia.com>
2019-03-28 09:31:41 -0700

    Added start time to output.

--- utils/nbtest.sh
+++ utils/nbtest.sh
@@ -14,7 +14,7 @@ for nb in $*; do
     echo --------------------
     cat ${NBFILENAME} | grep -v "%%" > ${NBTESTFILENAME}
     jupyter nbconvert --to script ${NBTESTFILENAME} --output /tmp/${NBNAME}-test
-    echo "Running \"python ${NBTESTSCRIPT}\":"
+    echo "Running \"python ${NBTESTSCRIPT}\" on $(date)"
     echo 
     time python ${NBTESTSCRIPT}
     NBEXITCODE=$?
