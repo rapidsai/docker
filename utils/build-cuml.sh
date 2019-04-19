46a7ea507f2634c7fd87fd049a1196ba9a6f831e
Rick Ratzel <rratzel@nvidia.com>
2019-04-18 20:58:35 -0700

    Initial commit of conda-installed-only base and runtime images.

--- utils/build-cuml.sh
+++ utils/build-cuml.sh
@@ -8,10 +8,12 @@ if [[ ${CUDA_VERSION} != "9.2" ]]; then
     MULTIGPU_OPTION=--multigpu
 fi
 
+# FIXME: -DCMAKE_BUILD_TYPE should be set to release! This is working
+# around a cuML bug.
 cd cuml/cuML && mkdir -p build && cd build && \
 cmake -DCMAKE_INSTALL_PREFIX=$CONDA_PREFIX \
       -DCMAKE_CXX11_ABI=ON \
-      -DCMAKE_BUILD_TYPE=release .. && \
+      -DCMAKE_BUILD_TYPE=debug .. && \
 make -j && make -j install && \
 cd ../../python && python setup.py build_ext --inplace ${MULTIGPU_OPTION} && \
 python setup.py install ${MULTIGPU_OPTION}
