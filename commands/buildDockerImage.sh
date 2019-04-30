9736d0be6cb143ad4680804ffde3e0ca9853dbac
Rick Ratzel <rratzel@nvidia.com>
2019-04-30 13:34:17 -0700

    Added new template for CentOS gpuCI images.  Updated config to add varaibles and added supportfiles used by gpuCI.

--- commands/buildDockerImage.sh
+++ commands/buildDockerImage.sh
@@ -114,7 +114,10 @@ mkdir -p ${RAPIDS_SOURCES_DIR}
 
 # Copy other dirs that many Dockerfiles expect to copy from their CWD
 # FIXME: ensure the copy succeeds
+# FIXME: copy the .condarc files directly to the OUTPUT_DIR since
+# certain Dockerfiles require this.
 cp -a ${RAPIDSDEVTOOL_DIR}/supportfiles ${OUTPUT_DIR}
+cp -a ${RAPIDSDEVTOOL_DIR}/supportfiles/.condarc* ${OUTPUT_DIR}
 cp -a ${RAPIDSDEVTOOL_DIR}/utils ${OUTPUT_DIR}
 
 # Generate the Dockerfile
