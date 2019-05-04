b045acb77e5ef48ba7fed5beae1bc3d7af8d4793
Rick Ratzel <rratzel@nvidia.com>
2019-05-04 08:44:14 -0700

    Updated cudf build steps, removed RMM parameter from config, changed .a to .so for cuml libs

--- utils/build-cudf.sh
+++ utils/build-cudf.sh
@@ -5,6 +5,5 @@ cmake -DCMAKE_INSTALL_PREFIX=$CONDA_PREFIX \
       -DCMAKE_CXX11_ABI=ON \
       -DCMAKE_BUILD_TYPE=release .. && \
 make -j && make -j install && \
-make python_cffi && make install_python && \
 cd ../../python && python setup.py build_ext --inplace && \
 python setup.py install
