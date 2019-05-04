7a29c5d936de021017ee399988cdde18eb0932ba
Rick Ratzel <rratzel@nvidia.com>
2019-05-04 11:36:01 -0700

    Temporarily not using --remote for cudf since it results in a -dirty version tag.

--- commands/utils/dumpRapidsCloneCmdsFromConfig.sh
+++ commands/utils/dumpRapidsCloneCmdsFromConfig.sh
@@ -62,7 +62,7 @@ awk -v "debug=${DEBUG}" '
           branch = repobranches[repo]
           # NOTE: THIS ASSUMES FUNCTIONS clone AND shouldClone ARE DEFINED!
           # FIXME: This is a hack to support cuML and xgboost until they change/fix how they use submodules
-          if ((dir == "cuml") || (dir == "xgboost")) {
+          if ((dir == "cudf") || (dir == "cuml") || (dir == "xgboost")) {
              printf("if shouldClone %s; then\n   clone %s %s %s noremote\nfi\n", dir, url, dir, branch)
           } else {
              printf("if shouldClone %s; then\n   clone %s %s %s\nfi\n", dir, url, dir, branch)
