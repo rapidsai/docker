cf53c27d2bdabfc68c7744b8dcd0a913c4dfd13b
Rick Ratzel <rratzel@nvidia.com>
2019-05-07 09:05:36 -0700

    Renamed var in test.sh to be more descriptive, added several packages (likely) needed to run NBs to runtime image.

--- supportfiles/test.sh
+++ supportfiles/test.sh
@@ -4,7 +4,7 @@ set -ex
 RAPIDS_DIR=/rapids
 NOTEBOOKS_DIR=${RAPIDS_DIR}/notebooks
 NBTEST=${RAPIDS_DIR}/utils/nbtest.sh
-NBDIRS="cuml cugraph"
+NOTEBOOK_DIR_NAMES="cuml cugraph"
 
 ## Check env
 env
@@ -14,9 +14,9 @@ source activate rapids
 
 EXITCODE=0
 
-# Always run nbtest in all NBDIRS, set EXITCODE to failure if any run
-# fails
-for nbdirname in ${NBDIRS}; do
+# Always run nbtest in all NOTEBOOK_DIR_NAMES, set EXITCODE to failure
+# if any run fails
+for nbdirname in ${NOTEBOOK_DIR_NAMES}; do
     cd ${NOTEBOOKS_DIR}/${nbdirname}
     ${NBTEST} *.ipynb
     EXITCODE=$((EXITCODE | $?))
