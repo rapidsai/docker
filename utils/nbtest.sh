05b878188c503672ffadf505984eed6d36cd82cf
Rick Ratzel <rratzel@nvidia.com>
2019-05-09 02:09:18 -0700

    Added code to override the magic functions instead of remove them, since many contain side effects that some NBs depend on.

--- utils/nbtest.sh
+++ utils/nbtest.sh
@@ -1,19 +1,41 @@
 #!/bin/bash
 
+MAGIC_OVERRIDE_CODE="
+def my_run_line_magic(*args, **kwargs):
+    g=globals()
+    l={}
+    for a in args:
+        try:
+            exec(str(a),g,l)
+        except:
+            pass
+        else:
+            g.update(l)
+
+def my_run_cell_magic(*args, **kwargs):
+    my_run_line_magic(*args, **kwargs)
+
+get_ipython().run_line_magic=my_run_line_magic
+get_ipython().run_cell_magic=my_run_cell_magic
+
+"
+
 EXITCODE=0
 
 for nb in $*; do
     NBFILENAME=$1
     NBNAME=$(echo ${NBFILENAME}|cut -d'.' -f1)
-    NBTESTFILENAME=/tmp/${NBNAME}-test.ipynb
     NBTESTSCRIPT=/tmp/${NBNAME}-test.py
     shift
 
     echo --------------------
     echo ${NBNAME}
     echo --------------------
-    cat ${NBFILENAME} | grep -v "%%" > ${NBTESTFILENAME}
-    jupyter nbconvert --to script ${NBTESTFILENAME} --output /tmp/${NBNAME}-test
+    jupyter nbconvert --to script ${NBFILENAME} --output /tmp/${NBNAME}-test
+    echo "${MAGIC_OVERRIDE_CODE}" > /tmp/tmpfile
+    cat ${NBTESTSCRIPT} >> /tmp/tmpfile
+    mv /tmp/tmpfile ${NBTESTSCRIPT}
+
     echo "Running \"ipython ${NBTESTSCRIPT}\" on $(date)"
     echo 
     time ipython ${NBTESTSCRIPT}
