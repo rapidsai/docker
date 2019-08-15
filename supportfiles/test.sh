928407d8cefdcfa848656072163f6b3688078534
Rick Ratzel <rratzel@nvidia.com>
2019-08-15 13:27:18 -0700

    Updated NB scripts to run additional NBs (cuml, xgb, tutorials) and skip specific ones, added install of libcumlprims.

--- supportfiles/test.sh
+++ supportfiles/test.sh
@@ -3,8 +3,9 @@
 RAPIDS_DIR=/rapids
 NOTEBOOKS_DIR=${RAPIDS_DIR}/notebooks
 NBTEST=${RAPIDS_DIR}/utils/nbtest.sh
-NOTEBOOK_DIR_NAMES="cuml cugraph"
+NOTEBOOK_DIR_NAMES="cuml cugraph cudf xgboost tutorials"
 
+SKIPNBS="kmeans_demo_mnmg.ipynb random_forest_mnmg_demo.ipynb"
 ## Check env
 env
 
@@ -13,9 +14,19 @@ EXITCODE=0
 # Always run nbtest in all NOTEBOOK_DIR_NAMES, set EXITCODE to failure
 # if any run fails
 for nbdirname in ${NOTEBOOK_DIR_NAMES}; do
-    cd ${NOTEBOOKS_DIR}/${nbdirname}
-    ${NBTEST} *.ipynb
-    EXITCODE=$((EXITCODE | $?))
+    echo "========================================"
+    echo "  ${nbdirname}"
+    echo "========================================"
+    pushd ${NOTEBOOKS_DIR}/${nbdirname} > /dev/null
+    for nb in $(ls *.ipynb); do
+        if (echo " ${SKIPNBS} " | grep -q " ${nb} "); then
+            echo "SKIPPING ${nb}"
+        else
+            ${NBTEST} ${nb}
+            EXITCODE=$((EXITCODE | $?))
+        fi
+    done
+    popd > /dev/null
 done
 
 exit ${EXITCODE}
