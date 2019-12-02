2ff822d69e377c050f8e8db9842cb91bc23c08b3
Rick Ratzel <rratzel@nvidia.com>
2019-12-02 11:47:46 -0600

    Added script to test notebooks-contrib

---
+++ supportfiles/test-nbcontrib.sh
@@ -0,0 +1,31 @@
+NCFOLDER=/notebooks-contrib
+EXITCODE=0
+NBTEST=${RAPIDS_DIR}/utils/nbtest.sh
+
+cd /
+git clone https://github.com/rapidsai/notebooks-contrib.git
+cd ${NCFOLDER}
+
+FOLDERS=$(find . -name *.ipynb |cut -d'/' -f2|sort -u)
+
+for folder in ${FOLDERS}; do
+    echo "========================================"
+    echo "FOLDER: ${folder}"
+    echo "========================================"
+    cd ${folder}
+    for nb in $(find . -name *.ipynb); do
+        nbBasename=$(basename ${nb})
+        if (echo ${nb}|grep -qi dask); then
+            echo "--------------------------------------------------------------------------------"
+            echo "SKIPPING: ${nbBasename}"
+            echo "--------------------------------------------------------------------------------"
+        fi
+        cd $(dirname ${nb})
+        ${NBTEST} ${nbBasename}
+        EXITCODE=$((EXITCODE | $?))
+        cd ${NCFOLDER}/${folder}
+   done
+   cd ${NCFOLDER}
+done
+
+exit ${EXITCODE}
