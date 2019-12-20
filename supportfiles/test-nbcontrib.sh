3f734ce527c1024d5356d2256e93bab358c19020
Rick Ratzel <rratzel@nvidia.com>
2019-12-20 15:07:22 -0600

    Added librdkafka build dep to devel, made test and test-contrib NB test scripts consistent (mainly so test could use a find-based NB discovery like contrib to find NBs in sub-folders)

--- supportfiles/test-nbcontrib.sh
+++ supportfiles/test-nbcontrib.sh
@@ -1,46 +1,58 @@
-NCFOLDER=/notebooks-contrib
-EXITCODE=0
+#!/bin/bash
+
+RAPIDS_DIR=/rapids
+NOTEBOOKS_DIR=${RAPIDS_DIR}/notebooks-contrib
 NBTEST=${RAPIDS_DIR}/utils/nbtest.sh
 
-cd /
+cd $(dirname ${NOTEBOOKS_DIR})
 git clone https://github.com/rapidsai/notebooks-contrib.git
-cd ${NCFOLDER}
+cd ${NOTEBOOKS_DIR}
+TOPLEVEL_NB_FOLDERS=$(find . -name *.ipynb |cut -d'/' -f2|sort -u)
 
-FOLDERS=$(find . -name *.ipynb |cut -d'/' -f2|sort -u)
-
-# louvain_benchmark.ipynb pagerank_benchmark.ipynb - timedout
+# Add notebooks that should be skipped here
+# (space-separated list of filenames without paths)
+#
+# louvain_benchmark.ipynb and pagerank_benchmark.ipynb - timedout
 # https://gpuci.gpuopenanalytics.com/job/docker/job/tests/job/docker-test-notebooks-contrib/17/CUDA_VERSION=10.0,LINUX_VERSION=ubuntu18.04/consoleFull
 # cuml_benchmarks.ipynb - crash/hang on CentOS7 CUDA 10.*
 # https://gpuci.gpuopenanalytics.com/job/docker/job/tests/job/docker-test-notebooks-contrib/24
 
 SKIPNBS="louvain_benchmark.ipynb pagerank_benchmark.ipynb cuml_benchmarks.ipynb"
 
-for folder in ${FOLDERS}; do
+## Check env
+env
+
+EXITCODE=0
+
+# Always run nbtest in all TOPLEVEL_NB_FOLDERS, set EXITCODE to failure
+# if any run fails
+for folder in ${TOPLEVEL_NB_FOLDERS}; do
     echo "========================================"
     echo "FOLDER: ${folder}"
     echo "========================================"
-    cd ${folder}
-    for nb in $(find . -name *.ipynb); do
+    cd ${NOTEBOOKS_DIR}/${folder}
+    for nb in $(find . -name "*.ipynb"); do
         nbBasename=$(basename ${nb})
         # Skip all NBs that use dask (in the code or even in their name)
         if ((echo ${nb}|grep -qi dask) || \
             (grep -q dask ${nb})); then
             echo "--------------------------------------------------------------------------------"
-            echo "SKIPPING: ${nbBasename} (suspected Dask usage, not currently automatable)"
+            echo "SKIPPING: ${nb} (suspected Dask usage, not currently automatable)"
             echo "--------------------------------------------------------------------------------"
         elif (echo " ${SKIPNBS} " | grep -q " ${nbBasename} "); then
             echo "--------------------------------------------------------------------------------"
-            echo "SKIPPING: ${nbBasename} (listed in skip list)"
+            echo "SKIPPING: ${nb} (listed in skip list)"
             echo "--------------------------------------------------------------------------------"
         else
             cd $(dirname ${nb})
             nvidia-smi
             ${NBTEST} ${nbBasename}
             EXITCODE=$((EXITCODE | $?))
-            cd ${NCFOLDER}/${folder}
+            cd ${NOTEBOOKS_DIR}/${folder}
         fi
    done
-   cd ${NCFOLDER}
 done
+
+nvidia-smi
 
 exit ${EXITCODE}
--- supportfiles/test.sh
+++ supportfiles/test.sh
@@ -3,40 +3,47 @@
 RAPIDS_DIR=/rapids
 NOTEBOOKS_DIR=${RAPIDS_DIR}/notebooks
 NBTEST=${RAPIDS_DIR}/utils/nbtest.sh
-NOTEBOOK_DIR_NAMES="cudf cuml cugraph xgboost tutorials"
 
-SKIPNBS="kmeans_demo_mnmg.ipynb random_forest_mnmg_demo.ipynb"
+cd ${NOTEBOOKS_DIR}
+TOPLEVEL_NB_FOLDERS=$(find . -name *.ipynb |cut -d'/' -f2|sort -u)
+
+# Add notebooks that should be skipped here
+# (space-separated list of filenames without paths)
+
+SKIPNBS=""
+
 ## Check env
 env
 
 EXITCODE=0
 
-# Always run nbtest in all NOTEBOOK_DIR_NAMES, set EXITCODE to failure
+# Always run nbtest in all TOPLEVEL_NB_FOLDERS, set EXITCODE to failure
 # if any run fails
-for nbdirname in ${NOTEBOOK_DIR_NAMES}; do
+for folder in ${TOPLEVEL_NB_FOLDERS}; do
     echo "========================================"
-    echo "FOLDER: ${nbdirname}"
+    echo "FOLDER: ${folder}"
     echo "========================================"
-    pushd ${NOTEBOOKS_DIR}/${nbdirname} > /dev/null
-    for nb in $(ls *.ipynb); do
+    cd ${NOTEBOOKS_DIR}/${folder}
+    for nb in $(find . -name "*.ipynb"); do
         nbBasename=$(basename ${nb})
         # Skip all NBs that use dask (in the code or even in their name)
         if ((echo ${nb}|grep -qi dask) || \
             (grep -q dask ${nb})); then
             echo "--------------------------------------------------------------------------------"
-            echo "SKIPPING: ${nbBasename} (suspected Dask usage, not currently automatable)"
+            echo "SKIPPING: ${nb} (suspected Dask usage, not currently automatable)"
             echo "--------------------------------------------------------------------------------"
         elif (echo " ${SKIPNBS} " | grep -q " ${nbBasename} "); then
             echo "--------------------------------------------------------------------------------"
-            echo "SKIPPING: ${nbBasename} (listed in skip list)"
+            echo "SKIPPING: ${nb} (listed in skip list)"
             echo "--------------------------------------------------------------------------------"
         else
+            cd $(dirname ${nb})
             nvidia-smi
-            ${NBTEST} ${nb}
+            ${NBTEST} ${nbBasename}
             EXITCODE=$((EXITCODE | $?))
+            cd ${NOTEBOOKS_DIR}/${folder}
         fi
     done
-    popd > /dev/null
 done
 
 nvidia-smi
