1dd10a1b4c26e404635b8c955b23d462ac824a87
dillon-cullinan <dcullinan92@gmail.com>
2020-03-13 10:59:02 -0700

    Update notebooks test script for new notebooks repo, build legacy tests for cudf

--- supportfiles/test.sh
+++ supportfiles/test.sh
@@ -5,7 +5,6 @@ NBTEST=${RAPIDS_DIR}/utils/nbtest.sh
 LIBCUDF_KERNEL_CACHE_PATH=${WORKSPACE}/.jitcache
 
 cd ${RAPIDS_DIR}
-TOPLEVEL_NB_FOLDERS=$(ls */notebooks* | grep -i ":" | sed s/://)
 
 # Add notebooks that should be skipped here
 # (space-separated list of filenames without paths)
@@ -19,32 +18,26 @@ EXITCODE=0
 
 # Always run nbtest in all TOPLEVEL_NB_FOLDERS, set EXITCODE to failure
 # if any run fails
-for folder in ${TOPLEVEL_NB_FOLDERS}; do
-    echo "========================================"
-    echo "FOLDER: ${folder}"
-    echo "========================================"
-    cd ${RAPIDS_DIR}/${folder} 
-    for nb in $(find . -name "*.ipynb"); do
-        nbBasename=$(basename ${nb})
-        # Skip all NBs that use dask (in the code or even in their name)
-        if ((echo ${nb}|grep -qi dask) || \
-            (grep -q dask ${nb})); then
-            echo "--------------------------------------------------------------------------------"
-            echo "SKIPPING: ${nb} (suspected Dask usage, not currently automatable)"
-            echo "--------------------------------------------------------------------------------"
-        elif (echo " ${SKIPNBS} " | grep -q " ${nbBasename} "); then
-            echo "--------------------------------------------------------------------------------"
-            echo "SKIPPING: ${nb} (listed in skip list)"
-            echo "--------------------------------------------------------------------------------"
-        else
-            cd $(dirname ${nb})
-            nvidia-smi
-            ${NBTEST} ${nbBasename}
-            EXITCODE=$((EXITCODE | $?))
-            rm -rf ${LIBCUDF_KERNEL_CACHE_PATH}/*
-            cd ${RAPIDS_DIR}/${folder}
-        fi
-    done
+for nb in $(find repos/*/notebooks/* -name *.ipynb); do
+    nbBasename=$(basename ${nb})
+    # Skip all NBs that use dask (in the code or even in their name)
+    if ((echo ${nb}|grep -qi dask) || \
+        (grep -q dask ${nb})); then
+        echo "--------------------------------------------------------------------------------"
+        echo "SKIPPING: ${nb} (suspected Dask usage, not currently automatable)"
+        echo "--------------------------------------------------------------------------------"
+    elif (echo " ${SKIPNBS} " | grep -q " ${nbBasename} "); then
+        echo "--------------------------------------------------------------------------------"
+        echo "SKIPPING: ${nb} (listed in skip list)"
+        echo "--------------------------------------------------------------------------------"
+    else 
+        cd $(dirname ${nb})
+        nvidia-smi
+        ${NBTEST} ${nbBasename}
+        EXITCODE=$((EXITCODE | $?))
+        rm -rf ${LIBCUDF_KERNEL_CACHE_PATH}/*
+        cd ${RAPIDS_DIR}/${folder}
+    fi
 done
 
 nvidia-smi
