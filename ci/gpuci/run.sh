ea32a38e45227188b3fa63552e8756c54d8722cc
Mike Wendt <mike.wendt@me.com>
2020-06-01 18:20:10 -0400

    FIX Add 'cuda' label to CUDA version in tag

--- ci/gpuci/run.sh
+++ ci/gpuci/run.sh
@@ -40,7 +40,7 @@ gpuci_logger ">>>> END Dockerfile <<<<"
 
 # Get build info ready
 gpuci_logger "Preparing build config..."
-BUILD_TAG="${CUDA_VER}-${IMAGE_TYPE}-${LINUX_VER}"
+BUILD_TAG="cuda${CUDA_VER}-${IMAGE_TYPE}-${LINUX_VER}"
 BUILD_ARGS="--squash --build-arg FROM_IMAGE=$FROM_IMAGE --build-arg CUDA_VER=$CUDA_VER --build-arg IMAGE_TYPE=$IMAGE_TYPE --build-arg LINUX_VER=$LINUX_VER"
 # Check if PYTHON_VER is set
 if [ -z "$PYTHON_VER" ] ; then
