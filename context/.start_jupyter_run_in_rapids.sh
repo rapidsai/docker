52a0e72b44ee1dc14d88263d8a36c6bbf58ad3d3
Aleksander Ficek <37374704+aleksficek@users.noreply.github.com>
2020-07-08 13:34:40 -0400

    Update .start_jupyter_run_in_rapids.sh

--- context/.start_jupyter_run_in_rapids.sh
+++ context/.start_jupyter_run_in_rapids.sh
@@ -2,5 +2,5 @@
 source activate rapids
 /rapids/utils/start_jupyter.sh > /dev/null 
 echo "Notebook server successfully started!"
-echo "To access, head to http://localhost:8888 on your host machine."
+echo "To access head to http://localhost:8888 on your host machine."
 exec "$@" 
