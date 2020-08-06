4cc1b5f77be95ce963898778621263b699e108b8
AJ Schmidt <aschmidt@nvidia.com>
2020-08-06 20:48:03 +0000

    pipe jupyter lab output to file

--- context/start_jupyter.sh
+++ context/start_jupyter.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-nohup jupyter-lab --allow-root --ip=0.0.0.0 --no-browser --NotebookApp.token='' > /dev/null 2>&1 &
+nohup jupyter-lab --allow-root --ip=0.0.0.0 --no-browser --NotebookApp.token='' > .jupyter_lab_output.txt 2>&1 &
 echo -e "\n"
-echo "nohup jupyter-lab --allow-root --ip=0.0.0.0 --no-browser --NotebookApp.token='' > /dev/null 2>&1 &"
+echo "nohup jupyter-lab --allow-root --ip=0.0.0.0 --no-browser --NotebookApp.token='' > .jupyter_lab_output.txt 2>&1 &"
 echo -e "\n"
