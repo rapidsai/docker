29249baca1388388a9bfa309760815f1e1279c86
AJ Schmidt <aschmidt@nvidia.com>
2020-08-07 20:16:54 +0000

    enable jupyter-lab to run in fg

--- context/start_jupyter.sh
+++ context/start_jupyter.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
-nohup jupyter-lab --allow-root --ip=0.0.0.0 --no-browser --NotebookApp.token='' > .jupyter_lab_output.txt 2>&1 &
+
+# Run Jupyter in foreground if $JUPYTER_FG is set
+if [[ -v JUPYTER_FG ]]; then
+   jupyter-lab --allow-root --ip=0.0.0.0 --no-browser --NotebookApp.token=''
+   exit 0
+fi
+
+nohup jupyter-lab --allow-root --ip=0.0.0.0 --no-browser --NotebookApp.token='' > /dev/null 2>&1 &
 echo -e "\n"
-echo "nohup jupyter-lab --allow-root --ip=0.0.0.0 --no-browser --NotebookApp.token='' > .jupyter_lab_output.txt 2>&1 &"
+echo "nohup jupyter-lab --allow-root --ip=0.0.0.0 --no-browser --NotebookApp.token='' > /dev/null 2>&1 &"
 echo -e "\n"
