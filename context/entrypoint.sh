8e8a90df228e508e15b25a240a5c089d8d846ffa
AJ Schmidt <aschmidt@nvidia.com>
2021-04-08 14:35:11 -0400

    add EULA

--- context/entrypoint.sh
+++ context/entrypoint.sh
@@ -4,6 +4,13 @@
 . /opt/conda/etc/profile.d/conda.sh
 conda activate rapids
 
+cat << EOF
+This container image and its contents are governed by the NVIDIA Deep Learning Container License.
+By pulling and using the container, you accept the terms and conditions of this license:
+https://developer.download.nvidia.com/licenses/NVIDIA_Deep_Learning_Container_License.pdf
+
+EOF
+
 source /opt/docker/bin/packages.sh
 
 # Source "source" file if it exists
