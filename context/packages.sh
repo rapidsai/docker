82ba8a08dcc27c191b6fa21059b63d44c0cd5f5a
Jacob Tomlinson <jtomlinson@nvidia.com>
2021-09-21 12:30:43 +0100

    Add explicit calls to exit on failure

--- context/packages.sh
+++ context/packages.sh
@@ -8,7 +8,7 @@ if [ "$EXTRA_APT_PACKAGES" ]; then
     fi
     echo "EXTRA_APT_PACKAGES environment variable found. Installing packages."
     apt update -y
-    timeout ${APT_TIMEOUT:-600} apt install -y --no-install-recommends $EXTRA_APT_PACKAGES
+    timeout ${APT_TIMEOUT:-600} apt install -y --no-install-recommends $EXTRA_APT_PACKAGES || exit 1
 fi
 
 if [ "$EXTRA_YUM_PACKAGES" ]; then
@@ -19,20 +19,20 @@ if [ "$EXTRA_YUM_PACKAGES" ]; then
     fi
     echo "EXTRA_YUM_PACKAGES environment variable found. Installing packages."
     yum check-update -y
-    timeout ${YUM_TIMEOUT:-600} yum install -y $EXTRA_YUM_PACKAGES
+    timeout ${YUM_TIMEOUT:-600} yum install -y $EXTRA_YUM_PACKAGES || exit 1
 fi
 
 if [ -e "/opt/rapids/environment.yml" ]; then
     echo "environment.yml found. Installing packages"
-    timeout ${CONDA_TIMEOUT:-600} conda env update -f /opt/rapids/environment.yml
+    timeout ${CONDA_TIMEOUT:-600} conda env update -f /opt/rapids/environment.yml || exit 1
 fi
 
 if [ "$EXTRA_CONDA_PACKAGES" ]; then
     echo "EXTRA_CONDA_PACKAGES environment variable found. Installing packages."
-    timeout ${CONDA_TIMEOUT:-600} conda install -y $EXTRA_CONDA_PACKAGES
+    timeout ${CONDA_TIMEOUT:-600} conda install -y $EXTRA_CONDA_PACKAGES || exit 1
 fi
 
 if [ "$EXTRA_PIP_PACKAGES" ]; then
     echo "EXTRA_PIP_PACKAGES environment variable found. Installing packages.".
-    timeout ${PIP_TIMEOUT:-600} pip install $EXTRA_PIP_PACKAGES
+    timeout ${PIP_TIMEOUT:-600} pip install $EXTRA_PIP_PACKAGES || exit 1
 fi
