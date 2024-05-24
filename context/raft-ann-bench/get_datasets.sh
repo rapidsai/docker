01efafd803bab8e1a0b0e189890fb757db7fb920
Divye Gala <divyegala@gmail.com>
2024-05-24 09:14:59 -0400

    Change `raft-ann-bench` package name to `raft_ann_bench` (#671)
    Depends on https://github.com/rapidsai/raft/pull/2333
    
    Authors:
      - Divye Gala (https://github.com/divyegala)
    
    Approvers:
      - Bradley Dice (https://github.com/bdice)
      - Jake Awe (https://github.com/AyodeAwe)
    
    URL: https://github.com/rapidsai/docker/pull/671

--- context/raft-ann-bench/get_datasets.sh
+++ context/raft-ann-bench/get_datasets.sh
@@ -5,10 +5,10 @@ set -eo pipefail
 
 export CONDA_PREFIX=/opt/conda
 
-python -m raft-ann-bench.get_dataset --dataset deep-image-96-angular --normalize --dataset-path /home/rapids/preloaded_datasets
-python -m raft-ann-bench.get_dataset --dataset fashion-mnist-784-euclidean --dataset-path /home/rapids/preloaded_datasets
-python -m raft-ann-bench.get_dataset --dataset glove-50-angular --normalize --dataset-path /home/rapids/preloaded_datasets
-python -m raft-ann-bench.get_dataset --dataset glove-100-angular --normalize --dataset-path /home/rapids/preloaded_datasets
-python -m raft-ann-bench.get_dataset --dataset mnist-784-euclidean --dataset-path /home/rapids/preloaded_datasets
-python -m raft-ann-bench.get_dataset --dataset nytimes-256-angular --normalize --dataset-path /home/rapids/preloaded_datasets
-python -m raft-ann-bench.get_dataset --dataset sift-128-euclidean --dataset-path /home/rapids/preloaded_datasets
+python -m raft_ann_bench.get_dataset --dataset deep-image-96-angular --normalize --dataset-path /home/rapids/preloaded_datasets
+python -m raft_ann_bench.get_dataset --dataset fashion-mnist-784-euclidean --dataset-path /home/rapids/preloaded_datasets
+python -m raft_ann_bench.get_dataset --dataset glove-50-angular --normalize --dataset-path /home/rapids/preloaded_datasets
+python -m raft_ann_bench.get_dataset --dataset glove-100-angular --normalize --dataset-path /home/rapids/preloaded_datasets
+python -m raft_ann_bench.get_dataset --dataset mnist-784-euclidean --dataset-path /home/rapids/preloaded_datasets
+python -m raft_ann_bench.get_dataset --dataset nytimes-256-angular --normalize --dataset-path /home/rapids/preloaded_datasets
+python -m raft_ann_bench.get_dataset --dataset sift-128-euclidean --dataset-path /home/rapids/preloaded_datasets
