9a651b2f92156caca52b9d9fd86bbed1aa22fd5c
James Lamb <jlamb@nvidia.com>
2024-05-24 17:18:04 -0500

    executable is called container-canary when you 'go install' it

--- ci/container-canary/run-checks.sh
+++ ci/container-canary/run-checks.sh
@@ -5,11 +5,11 @@ set -e -u
 IMAGE_URI="${1}"
 
 # using a config checked in here
-canary validate \
+container-canary validate \
     --file ./ci/container-canary/rapids.yml \
     "${IMAGE_URI}"
 
 # usage a config from the container-canary repo
-canary validate \
+container-canary validate \
     --file https://raw.githubusercontent.com/NVIDIA/container-canary/main/examples/databricks.yaml \
     "${IMAGE_URI}"
