2c51893f590b0798bfd06eafbb7905265ec0e4f5
James Lamb <jlamb@nvidia.com>
2024-05-28 13:01:52 -0500

    try to get a pass

--- ci/container-canary/run-checks.sh
+++ ci/container-canary/run-checks.sh
@@ -3,6 +3,7 @@
 set -e -u
 
 IMAGE_URI="${1}"
+IMAGE_URI="ubuntu:latest"
 
 # pre-pull
 docker pull "${IMAGE_URI}"
@@ -10,7 +11,7 @@ docker pull "${IMAGE_URI}"
 # using a config checked in here
 container-canary validate \
     --file ./ci/container-canary/rapids.yml \
-    ubuntu:latest
+    "${IMAGE_URI}"
 
 # # usage a config from the container-canary repo
 # container-canary validate \
