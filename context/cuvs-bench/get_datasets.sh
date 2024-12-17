ca9196826ba6e73c8675b8626f0d58bbd6ff6c46
Dante Gama Dessavre <danteg@nvidia.com>
2024-12-17 14:05:21 -0600

    FIX wrong package name

--- context/cuvs-bench/get_datasets.sh
+++ context/cuvs-bench/get_datasets.sh
@@ -4,7 +4,7 @@
 set -eo pipefail
 
 # find cuVS in the environment
-PACKAGE_FILE_PATH=$(python -c "import cuvs_bench; print(package_name.__file__)")
+PACKAGE_FILE_PATH=$(python -c "import cuvs_bench; print(cuvs_bench.__file__)")
 PACKAGE_DIR=$(dirname "$PACKAGE_FILE_PATH")
 
 # Apply the patch
