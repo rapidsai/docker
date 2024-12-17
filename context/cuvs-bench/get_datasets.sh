d3ad5f24de7006b54579278eaf108249a5054505
Ray Douglass <3107146+raydouglass@users.noreply.github.com>
2024-12-17 15:44:18 -0500

    Fix patch file path

--- context/cuvs-bench/get_datasets.sh
+++ context/cuvs-bench/get_datasets.sh
@@ -8,7 +8,7 @@ PACKAGE_FILE_PATH=$(python -c "import cuvs_bench; print(cuvs_bench.__file__)")
 PACKAGE_DIR=$(dirname "$PACKAGE_FILE_PATH")
 
 # Apply the patch
-patch "$PACKAGE_DIR/get_dataset/__main__.py" < cuvs_bench_get_dataset.patch
+patch "$PACKAGE_DIR/get_dataset/__main__.py" < /home/rapids/cuvs-bench/cuvs_bench_get_dataset.patch
 
 python -m cuvs_bench.get_dataset --dataset deep-image-96-angular --normalize --dataset-path /home/rapids/preloaded_datasets
 python -m cuvs_bench.get_dataset --dataset fashion-mnist-784-euclidean --dataset-path /home/rapids/preloaded_datasets
