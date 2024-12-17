45eebf052192cde7579ac32ee879c816e1510134
Dante Gama Dessavre <danteg@nvidia.com>
2024-12-17 13:40:48 -0600

    FIX fixes from pr review and other small things

--- context/cuvs-bench/get_datasets.sh
+++ context/cuvs-bench/get_datasets.sh
@@ -4,7 +4,7 @@
 set -eo pipefail
 
 # find cuVS in the environment
-PACKAGE_FILE_PATH=$(python -c "import cuvs-bench; print(package_name.__file__)")
+PACKAGE_FILE_PATH=$(python -c "import cuvs_bench; print(package_name.__file__)")
 PACKAGE_DIR=$(dirname "$PACKAGE_FILE_PATH")
 
 # Apply the patch
