117d98eaacc5461054ff18e5bb633ca66378720e
Dante Gama Dessavre <danteg@nvidia.com>
2024-12-17 12:10:23 -0600

    FIX apply patch to use requests to download dataset

--- context/cuvs-bench/get_datasets.sh
+++ context/cuvs-bench/get_datasets.sh
@@ -3,6 +3,13 @@
 
 set -eo pipefail
 
+# find cuVS in the environment
+PACKAGE_FILE_PATH=$(python -c "import cuvs-bench; print(package_name.__file__)")
+PACKAGE_DIR=$(dirname "$PACKAGE_FILE_PATH")
+
+# Apply the patch
+patch "$PACKAGE_DIR/get_dataset/__main__.py" < cuvs_bench_get_dataset.patch
+
 python -m cuvs_bench.get_dataset --dataset deep-image-96-angular --normalize --dataset-path /home/rapids/preloaded_datasets
 python -m cuvs_bench.get_dataset --dataset fashion-mnist-784-euclidean --dataset-path /home/rapids/preloaded_datasets
 python -m cuvs_bench.get_dataset --dataset glove-50-angular --normalize --dataset-path /home/rapids/preloaded_datasets
