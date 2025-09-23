c24b0bbd5754e006acfa14eec6aca33e39bcb5c8
Bradley Dice <bdice@bradleydice.com>
2025-09-23 17:50:51 -0500

    Only build major version tags for CUDA 13.

--- ci/create-rapids-multiarch-manifest.sh
+++ ci/create-rapids-multiarch-manifest.sh
@@ -28,16 +28,22 @@ for arch in $(echo "${ARCHES}" | jq .[] -r); do
 done
 
 # Create and push Docker multi-arch manifests
-docker manifest create "${org}/${BASE_IMAGE_REPO}:${base_tag}" "${base_source_tags[@]}"
-docker manifest push "${org}/${BASE_IMAGE_REPO}:${base_tag}"
 
-docker manifest create "${org}/${NOTEBOOKS_IMAGE_REPO}:${notebooks_tag}" "${notebooks_source_tags[@]}"
-docker manifest push "${org}/${NOTEBOOKS_IMAGE_REPO}:${notebooks_tag}"
+# If CUDA 12.0 or 12.9, create and push multi-arch manifests with minor version tags
+if [[ "$CUDA_TAG" == "12.0" || "$CUDA_TAG" == "12.9" ]]; then
+    echo "Creating and pushing multi-arch manifests with minor version tags for cuda${CUDA_TAG}..."
 
-# If CUDA 12.9 or 13.0, retag images as cuda12 or cuda13
+    docker manifest create "${org}/${BASE_IMAGE_REPO}:${base_tag}" "${base_source_tags[@]}"
+    docker manifest push "${org}/${BASE_IMAGE_REPO}:${base_tag}"
+
+    docker manifest create "${org}/${NOTEBOOKS_IMAGE_REPO}:${notebooks_tag}" "${notebooks_source_tags[@]}"
+    docker manifest push "${org}/${NOTEBOOKS_IMAGE_REPO}:${notebooks_tag}"
+fi
+
+# If CUDA 12.9 or 13.0, create and push multi-arch manifests with major version tags
 if [[ "$CUDA_TAG" == "12.9" || "$CUDA_TAG" == "13.0" ]]; then
     cuda_major=${CUDA_TAG%.*}
-    echo "Retagging cuda${CUDA_TAG} images as cuda${cuda_major}..."
+    echo "Creating and pushing multi-arch manifests with major version tags for cuda${cuda_major}..."
 
     base_tag_cuda_major="${BASE_TAG_PREFIX}${RAPIDS_VER}${ALPHA_TAG}-cuda${cuda_major}-py${PYTHON_VER}"
     notebooks_tag_cuda_major="${NOTEBOOKS_TAG_PREFIX}${RAPIDS_VER}${ALPHA_TAG}-cuda${cuda_major}-py${PYTHON_VER}"
